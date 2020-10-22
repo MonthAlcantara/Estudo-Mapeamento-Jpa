353c5ccd98755dcfcc61164c40d685b42bec547b
monthalcantara <montival_junior@yahoo.com.br>
2020-10-22 18:37:31 -0300

    projeto de estudo criado

---
+++ src/main/java/io/github/monthalcantara/estudojpa/EstudoMapeamantoJpaApplication.java
@@ -0,0 +1,13 @@
+package io.github.monthalcantara.estudojpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudoMapeamantoJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstudoMapeamantoJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/monthalcantara/estudojpa/EstudoMapeamantoJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.monthalcantara.estudojpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudoMapeamantoJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
