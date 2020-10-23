7eb6194cd154c70116db837a51ef689a46b794cd
monthalcantara <montival_junior@yahoo.com.br>
2020-10-23 00:35:38 -0300

    implementada logica do CRUD de categoria para testar mapeamentos

--- src/main/java/io/github/monthalcantara/estudojpa/services/CategoriaService.java
+++ src/main/java/io/github/monthalcantara/estudojpa/services/CategoriaService.java
@@ -1,6 +1,10 @@
 package io.github.monthalcantara.estudojpa.services;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import io.github.monthalcantara.estudojpa.domain.Categoria;
@@ -14,6 +18,27 @@ public class CategoriaService {
 	
 	public Categoria buscar(Integer id) {
 	return categoriaRepository.findById(id).orElse(null);	
+	}
+
+	public Page<Categoria> buscarTodos(Pageable pageable) {
+		return categoriaRepository.findAll(pageable);
+	}
+
+	public Categoria salvarNovaCategoria(Categoria categoria) {
+		return categoriaRepository.save(categoria);
+	}
+
+	public void deletar(Integer id) {
+		categoriaRepository.deleteById(id);
+		
+	}
+
+	public Categoria atualizarCategoria(Integer id, Categoria categoria) {
+		Optional<Categoria> categoriaEncontrada = categoriaRepository.findById(id);
+		if(categoriaEncontrada.isPresent()){
+			categoria.setId(id);
+		}
+		return categoriaRepository.save(categoria);
 	}
 
 }
