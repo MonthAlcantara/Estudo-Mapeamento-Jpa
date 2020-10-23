59eb0c4a9f10759264c98faa33c621c8444dc49f
monthalcantara <montival_junior@yahoo.com.br>
2020-10-23 00:35:03 -0300

    implementada logica do CRUD de produto para testar mapeamentos

---
+++ src/main/java/io/github/monthalcantara/estudojpa/services/ProdutoService.java
@@ -0,0 +1,44 @@
+package io.github.monthalcantara.estudojpa.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import io.github.monthalcantara.estudojpa.domain.Produto;
+import io.github.monthalcantara.estudojpa.repositories.ProdutoRepository;
+
+@Service
+public class ProdutoService {
+
+	@Autowired
+	private ProdutoRepository produtoRepository;
+
+	public Page<Produto> buscarTodos(Pageable pageable) {
+		return produtoRepository.findAll(pageable);
+	}
+
+	public Produto buscar(Integer id) {
+		return produtoRepository.findById(id).orElse(null);
+	}
+
+	public Produto salvarNovaCategoria(Produto produto) {
+		return produtoRepository.save(produto);
+	}
+
+	public Produto atualizarCategoria(Integer id, Produto produto) {
+		Optional<Produto> produtoEncontrado = produtoRepository.findById(id);
+		if (produtoEncontrado.isPresent()) {
+			produto.setId(id);
+		}
+		return produtoRepository.save(produto);
+	}
+
+	public void deletar(Integer id) {
+		produtoRepository.deleteById(id);
+
+	}
+
+}
