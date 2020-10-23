1583c0691fb298138fc97f27c07eb01e105c9190
monthalcantara <montival_junior@yahoo.com.br>
2020-10-22 23:22:53 -0300

    Adicionado implementacao commandLineRunner

--- src/main/java/io/github/monthalcantara/estudojpa/EstudoMapeamantoJpaApplication.java
+++ src/main/java/io/github/monthalcantara/estudojpa/EstudoMapeamantoJpaApplication.java
@@ -1,13 +1,48 @@
 package io.github.monthalcantara.estudojpa;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class EstudoMapeamantoJpaApplication {
+import io.github.monthalcantara.estudojpa.domain.Categoria;
+import io.github.monthalcantara.estudojpa.domain.Produto;
+import io.github.monthalcantara.estudojpa.repositories.CategoriaRepository;
+import io.github.monthalcantara.estudojpa.repositories.ProdutoRepository;
 
+@SpringBootApplication
+public class EstudoMapeamantoJpaApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
+	@Autowired
+	private ProdutoRepository produtoRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(EstudoMapeamantoJpaApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informatica");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 	}
 
 }
