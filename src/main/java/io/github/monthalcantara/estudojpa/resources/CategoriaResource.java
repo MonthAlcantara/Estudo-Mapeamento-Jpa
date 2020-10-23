48ed90ef16b456130e3c0bbb1445aaa10c8e0da5
monthalcantara <montival_junior@yahoo.com.br>
2020-10-22 21:55:21 -0300

    Adicionado endpoint pesquisa categoria

--- src/main/java/io/github/monthalcantara/estudojpa/resources/CategoriaResource.java
+++ src/main/java/io/github/monthalcantara/estudojpa/resources/CategoriaResource.java
@@ -1,15 +1,25 @@
 package io.github.monthalcantara.estudojpa.resources;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import io.github.monthalcantara.estudojpa.domain.Categoria;
+import io.github.monthalcantara.estudojpa.services.CategoriaService;
 
 @RestController
 @RequestMapping("/categorias")
 public class CategoriaResource {
-	
-	@GetMapping
-	public String listar() {
-		return "Rest está funfando";
+
+	@Autowired
+	private CategoriaService categoriaService;
+
+	@GetMapping("/{id}")
+	public ResponseEntity buscaPeloId(@PathVariable Integer id) {
+		return ResponseEntity.ok(categoriaService.buscar(id));
 	}
 }
