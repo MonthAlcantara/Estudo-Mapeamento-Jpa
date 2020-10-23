d567306706983aab58dcf59fde6426fca39cf87d
monthalcantara <montival_junior@yahoo.com.br>
2020-10-23 13:46:28 -0300

    Adicionada criacao de cidades e estados para testes

--- src/main/java/io/github/monthalcantara/estudojpa/EstudoMapeamantoJpaApplication.java
+++ src/main/java/io/github/monthalcantara/estudojpa/EstudoMapeamantoJpaApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import io.github.monthalcantara.estudojpa.domain.Categoria;
+import io.github.monthalcantara.estudojpa.domain.Cidade;
+import io.github.monthalcantara.estudojpa.domain.Estado;
 import io.github.monthalcantara.estudojpa.domain.Produto;
 import io.github.monthalcantara.estudojpa.repositories.CategoriaRepository;
+import io.github.monthalcantara.estudojpa.repositories.CidadeRepository;
+import io.github.monthalcantara.estudojpa.repositories.EstadoRepository;
 import io.github.monthalcantara.estudojpa.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -20,6 +24,12 @@ public class EstudoMapeamantoJpaApplication implements CommandLineRunner {
 	
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	
+	@Autowired
+	private EstadoRepository estadoRepository;
+	
+	@Autowired
+	private CidadeRepository cidadeRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(EstudoMapeamantoJpaApplication.class, args);
@@ -43,6 +53,19 @@ public class EstudoMapeamantoJpaApplication implements CommandLineRunner {
 		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+		
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+		
+		Cidade c1 = new Cidade(null, "Uberlândia", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+		
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est1.getCidades().addAll(Arrays.asList(c2, c3));
+		
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 	}
 
 }
