4971b6bc3eab85632dabb40f7af227d0acf3b883
monthalcantara <montival_junior@yahoo.com.br>
2020-10-24 13:25:01 -0300

    adicionados novos recursos inicializacao banco

--- src/main/java/io/github/monthalcantara/estudojpa/EstudoMapeamantoJpaApplication.java
+++ src/main/java/io/github/monthalcantara/estudojpa/EstudoMapeamantoJpaApplication.java
@@ -9,10 +9,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import io.github.monthalcantara.estudojpa.domain.Categoria;
 import io.github.monthalcantara.estudojpa.domain.Cidade;
+import io.github.monthalcantara.estudojpa.domain.Cliente;
+import io.github.monthalcantara.estudojpa.domain.Endereco;
 import io.github.monthalcantara.estudojpa.domain.Estado;
 import io.github.monthalcantara.estudojpa.domain.Produto;
+import io.github.monthalcantara.estudojpa.domain.enums.TipoCliente;
 import io.github.monthalcantara.estudojpa.repositories.CategoriaRepository;
 import io.github.monthalcantara.estudojpa.repositories.CidadeRepository;
+import io.github.monthalcantara.estudojpa.repositories.ClienteRepository;
+import io.github.monthalcantara.estudojpa.repositories.EnderecoRepository;
 import io.github.monthalcantara.estudojpa.repositories.EstadoRepository;
 import io.github.monthalcantara.estudojpa.repositories.ProdutoRepository;
 
@@ -21,16 +26,22 @@ public class EstudoMapeamantoJpaApplication implements CommandLineRunner {
 
 	@Autowired
 	private CategoriaRepository categoriaRepository;
-	
+
 	@Autowired
 	private ProdutoRepository produtoRepository;
-	
+
 	@Autowired
 	private EstadoRepository estadoRepository;
-	
+
 	@Autowired
 	private CidadeRepository cidadeRepository;
-	
+
+	@Autowired
+	private ClienteRepository clienteRepository;
+
+	@Autowired
+	private EnderecoRepository enderecoRepository;
+
 	public static void main(String[] args) {
 		SpringApplication.run(EstudoMapeamantoJpaApplication.class, args);
 	}
@@ -39,33 +50,43 @@ public class EstudoMapeamantoJpaApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		Categoria cat1 = new Categoria(null, "Informatica");
 		Categoria cat2 = new Categoria(null, "Escritório");
-		
+
 		Produto p1 = new Produto(null, "Computador", 2000.00);
 		Produto p2 = new Produto(null, "Impressora", 800.00);
 		Produto p3 = new Produto(null, "Mouse", 80.00);
-		
+
 		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
 		cat2.getProdutos().addAll(Arrays.asList(p2));
-		
+
 		p1.getCategorias().addAll(Arrays.asList(cat1));
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
-		
+
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
-		
+
 		Estado est1 = new Estado(null, "Minas Gerais");
 		Estado est2 = new Estado(null, "São Paulo");
-		
+
 		Cidade c1 = new Cidade(null, "Uberlândia", est1);
 		Cidade c2 = new Cidade(null, "São Paulo", est2);
 		Cidade c3 = new Cidade(null, "Campinas", est2);
-		
+
 		est1.getCidades().addAll(Arrays.asList(c1));
 		est1.getCidades().addAll(Arrays.asList(c2, c3));
-		
+
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
-	}
 
+		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "36378912377", TipoCliente.PESSOAFISICA);
+		cli1.getTelefones().addAll(Arrays.asList("27363323", "9383839"));
+
+		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 303", "Jardim", "38220834", cli1, c1);
+		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "38777012", cli1, c2);
+
+		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
+
+		clienteRepository.saveAll(Arrays.asList(cli1));
+		enderecoRepository.saveAll(Arrays.asList(e1, e2));
+	}
 }
