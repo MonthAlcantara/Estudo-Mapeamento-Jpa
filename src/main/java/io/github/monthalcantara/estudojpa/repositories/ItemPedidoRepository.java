f710d795320f49febe1a36b93b3c55d453da0b9b
monthalcantara <montival_junior@yahoo.com.br>
2020-10-26 13:39:35 -0300

    Adicionados ItemPedido, ItemPedidoPK e repository

---
+++ src/main/java/io/github/monthalcantara/estudojpa/repositories/ItemPedidoRepository.java
@@ -0,0 +1,9 @@
+package io.github.monthalcantara.estudojpa.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import io.github.monthalcantara.estudojpa.domain.ItemPedido;
+
+public interface ItemPedidoRepository extends JpaRepository<ItemPedido, Integer> {
+
+}
