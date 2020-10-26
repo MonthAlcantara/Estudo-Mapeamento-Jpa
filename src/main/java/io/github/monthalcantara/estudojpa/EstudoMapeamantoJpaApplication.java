8eb426c665a5d6563fe2ffdc8de64e748259cd3a
monthalcantara <montival_junior@yahoo.com.br>
2020-10-26 10:28:55 -0300

    Novas insercoes de inicializacao banco

--- src/main/java/io/github/monthalcantara/estudojpa/EstudoMapeamantoJpaApplication.java
+++ src/main/java/io/github/monthalcantara/estudojpa/EstudoMapeamantoJpaApplication.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import io.github.monthalcantara.estudojpa.domain.Categoria;
 import io.github.monthalcantara.estudojpa.domain.Cidade;
