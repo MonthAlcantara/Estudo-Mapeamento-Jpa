21fda232178671a4eef10d42aa936c8c839c7232
monthalcantara <montival_junior@yahoo.com.br>
2020-10-27 13:33:29 -0300

    Adicionado dto de Categoria

---
+++ src/main/java/io/github/monthalcantara/estudojpa/dto/CategoriaDTO.java
@@ -0,0 +1,70 @@
+package io.github.monthalcantara.estudojpa.dto;
+
+import java.io.Serializable;
+
+import io.github.monthalcantara.estudojpa.domain.Categoria;
+
+public class CategoriaDTO implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private Integer id;
+	private String nome;
+
+	public CategoriaDTO() {
+
+	}
+
+	public CategoriaDTO(Categoria categoria) {
+		id = categoria.getId();
+		nome = categoria.getNome();
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		CategoriaDTO other = (CategoriaDTO) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		return true;
+	}
+	
+	
+}
