4f02e587dc459e7319aa4527fc64297bab59b99d
monthalcantara <montival_junior@yahoo.com.br>
2020-10-30 17:55:34 -0300

    Adicionado cascade para delecao de endereco

--- src/main/java/io/github/monthalcantara/estudojpa/domain/Cliente.java
+++ src/main/java/io/github/monthalcantara/estudojpa/domain/Cliente.java
@@ -37,7 +37,7 @@ public class Cliente implements Serializable {
 
 	private Integer tipo;
 
-	@OneToMany(mappedBy = "cliente")
+	@OneToMany(mappedBy = "cliente", cascade = CascadeType.ALL)
 	private List<Endereco> enderecos = new ArrayList<>();
 
 	@ElementCollection
